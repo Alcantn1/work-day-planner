b874ee92d46da700737e9c4d760dbe15081b15db
Alcantn1 <nalcantara92@gmail.com>
2023-05-18 19:18:55 -0700

    added read me

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# work-day-planner
+# Work Day Planner
+
+
+## Description
+This is a simple planner app that allows you to schedule your day by the hour to help you keep track of your daily activities. 
+
+## Installation
+No installation required.
+
+
+## Usage
+To use this site, all you need to do is open the web browser, enter the URL to deploy the website, click on the hour you would like to make plans for and simply hit the save button on the right side of the screen and it will save that activity so you can view your schedule for later.
+
+## Screenshot
+![screenshot of site](assets/planner.png)
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
+
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,69 @@
-$(document).ready(() => {
-  const today = dayjs();
-  setInterval( () => {
-  updateBlocks();
-},
- 60 *60 *1000);
-getInputData();
-
-  $('.saveBtn').on('click', () => {
-    const currentHour = dayjs(). hour();
+function updateBlocks() {
+  const currentHour = dayjs().hour();
+  $(".time-block").each(function() {
+    const timeBlockHour = parseInt($(this).attr("id"));
     
-    $(".time-block").each(function() {
-      const timeBlockHour = parseInt($(this).attr("id"));
+    if (timeBlockHour < currentHour) {
+      $(this).addClass("past");
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+  
+  
+    } else if (timeBlockHour === currentHour) {
+      $(this).addClass("present");
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+  
       
-      if (timeBlockHour < currentHour) {
-        $(this).addClass("past");
-        $(this).removeClass("present");
-        $(this).removeClass("future");
+    } else {
+      $(this).addClass("future");
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+    }
+    const input = $(this).find('.description').val();
+    const id = $(this).attr('id');
+    localStorage.setItem(id, input);
+  }
+  );
+  }
 
-
-      } else if (timeBlockHour === currentHour) {
-        $(this).addClass("present");
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-
-        
-      } else {
-        $(this).addClass("future");
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-      }
-      const input = $(this).find('.description').val();
+  function getInputData(){
+    $(".time-block").each(function() {
       const id = $(this).attr('id');
-      localStorage.setItem(id, input);
+      const input = localStorage.getItem(id);
+      $(this).find('.description').val(input);
+    });
+  }
+  $(document).ready(() => {
+    setInterval(updateBlocks, 60 * 60 * 1000);
+    getInputData();
+  
+    $('.saveBtn').on('click', () => {
+      const currentHour = dayjs().hour();
+      $(".time-block").each((index, element) => {
+        const timeBlockHour = parseInt($(element).attr("id"), 10);
+        $(element).removeClass("past present future");
+  
+        if (timeBlockHour < currentHour) {
+          $(element).addClass("past");
+        } else if (timeBlockHour === currentHour) {
+          $(element).addClass("present");
+        } else {
+          $(element).addClass("future");
+        }
+  
+        const input = $(element).find('.description').val();
+        const id = $(element).attr('id');
+        localStorage.setItem(id, input);
+      });
     });
   });
-});
+
+  
+
+
+
+
 
   
    
